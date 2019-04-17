942f5c442c8ebfe21240232e5805f23362f35a33
bbruff22 <brent.bruffett@yahoo.com>
2019-04-16 22:14:58 -0500

    correcting code

--- plot1.R
+++ plot1.R
@@ -1,13 +1,11 @@
 ######################################################################################
-# Exploratory Data Analysis - Project 1
+# Course 4, Exploratory Data Analysis - Project 1
 
-data <- read.table("./data/household_power_consumption.txt", stringsAsFactors = FALSE, sep = ";")
-dataFile <- "./data/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+# read and subset file to days we care about
+data <- read.table("./data/household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-#str(subSetData)
-globalActivePower <- as.numeric(subSetData$Global_active_power)
+globalActivePower <- as.numeric(subData$Global_active_power)
 png("plot1.png", width=480, height=480)
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off() 
