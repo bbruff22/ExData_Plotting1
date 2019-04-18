d618b7c15f8891ebe036d4a5b06410c9f75ab2ca
bbruff22 <brent.bruffett@yahoo.com>
2019-04-17 21:46:37 -0500

    cleaning up

--- plot1.R
+++ plot1.R
@@ -1,11 +1,13 @@
 ######################################################################################
-# Course 4, Exploratory Data Analysis - Project 1
+# Course 4, Exploratory Data Analysis - Project 1, plot1
 
 # read and subset file to days we care about
 data <- read.table("./data/household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-
-globalActivePower <- as.numeric(subData$Global_active_power)
+subsettedData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+# read plot data
+globalActivePower <- as.numeric(subsettedData$Global_active_power)
+# open png grahics device and set features of the histogram
 png("plot1.png", width=480, height=480)
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+# close the plot file
 dev.off() 
